0a850fe1ee07f7a9dc2cb7137818e9b81ff7ca83
Sandro Jenny <s.jenny@mac.com>
2018-03-13 08:34:16 +0100

    Removed Askimet Plugin

--- tfc_website/wp-content/themes/tfctheme/index.php
+++ tfc_website/wp-content/themes/tfctheme/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>the fundraising company – solutions to success.</title>
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
