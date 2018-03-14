2b7088425433157e871710f7d96639603e490ef7
Sandro Jenny <s.jenny@mac.com>
2018-03-14 10:01:42 +0100

    Templates with different Intro colors

---
+++ tfc_website/wp-content/themes/tfctheme/page-blue.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Blue Intro
+ *
+ * @package WordPress
+ * @subpackage TFC
+ * @since TFC 1.0
+ */
+?>
+
+<?php get_header() ?>
+
+  <div class="grid-container-fluid">
+    <div class="grid-x">
+      <div class="cell">
+        <div class="heroSection">
+          <?php
+          $image = get_field('hauptbild');
+          if( !empty($image) ): ?>
+          	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="grid-container-fluid">
+    <div class="grid-x">
+      <div class="cell">
+        <?php
+          // vars
+          $intro = get_field('intro');
+
+          if( $intro ): ?>
+            <div class="intro blue">
+              <div class="grid-container">
+                <div class="cell small-12 medium-12 large-12">
+                	<h1><?php echo $intro['titel']; ?></h1>
+                  <p><?php echo $intro['text']; ?></p>
+                </div>
+              </div>
+            </div>
+          <?php endif; ?>
+      </div>
+    </div>
+  </div>
+
+  <div class="grid-container content">
+      <div class="cell">
+        <div class="cell small-12 medium-12 large-12">
+        <?php the_field('inhalt'); ?>
+        </div>
+      </div>
+  </div>
+
+<?php get_footer() ?>
